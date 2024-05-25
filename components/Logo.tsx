181070ab3b94ac5030511e80f1b085f9a65f2b1a
Avinash Pandey <avinash.12009084@lpu.in>
2024-05-26 00:54:41 +0530

    APi and Cards done

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,10 @@
-import { RiArrowRightDoubleFill } from "@remixicon/react";
+import { RiArrowRightDoubleFill, RiSteering2Fill } from "@remixicon/react";
 import React from "react";
 
 const Logo = () => {
   return (
-    <p className="flex justify-center items-center text-xl font-bold">
-      car <span className="text-blue-600 font-extrabold text-2xl">genia</span>
+    <p className="flex justify-center items-center text-xl font-semibold">
+      RentN <span className="text-blue-600 font-extrabold text-3xl">Drive</span>
     </p>
   );
 };
