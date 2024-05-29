5105344b3baffc03f46236000f4a682a8386d162
Avinash Pandey <avinash.12009084@lpu.in>
2024-05-30 00:49:07 +0530

    Search Box

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,31 @@
 "use client";
 import React, { useState } from "react";
 import SearchManufacturer from "./SearchManufacturer";
+import { RiSearchLine } from "@remixicon/react";
+import Image from "next/image";
+
+const SearchButton = ({ otherClasses }: any) => {
+  return (
+    <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+      <RiSearchLine size={25} className="object-contain" />
+    </button>
+  );
+};
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
-  const handleSearch = () => {};
+  const [model, setModel] = useState("");
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (manufacturer === "" && model === "") {
+      return alert("Please fill in search bar");
+    }
+  };
+
+  const updateSearchParams = () => {};
+
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
@@ -13,6 +34,24 @@ const SearchBar = () => {
           setManufacturer={setManufacturer}
         />
       </div>
+      <div className="searchbar__item">
+        <Image
+          src={"/model-icon.png"}
+          width={25}
+          height={25}
+          alt="model"
+          className="absolute w-[20px] h-[20px] ml-4"
+        />
+        <input
+          type="text"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Taigun Model"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses={"sm:hidden"} />
+      </div>
+      <SearchButton otherClasses={"max-sm:hidden"} />
     </form>
   );
 };
